49ee3cc9af9bef3d743b3e14be0907767908f5c0
花果山大圣 <316783812@qq.com>
2023-01-28 14:34:41 +0800

    add link for twitter

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,14 +13,23 @@ export default function Footer() {
         >
           OpenAI{" "}
         </a>
-        and{" "}
+        {" + "}
         <a
           href="https://vercel.com/"
           target="_blank"
           rel="noreferrer"
           className="font-bold hover:underline transition underline-offset-2"
         >
-          Vercel Edge Functions.
+          Vercel
+        </a>
+        {" + "}
+        <a
+          href="https://nextjs.org/"
+          target="_blank"
+          rel="noreferrer"
+          className="font-bold hover:underline transition underline-offset-2"
+        >
+          Next.js
         </a>
       </div>
       <div className="flex space-x-4 pb-4 sm:pb-0">
