acadcebc829e5e84b86904b55cdcfbca8e931010
花果山大圣 <316783812@qq.com>
2023-01-29 14:53:31 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Analytics } from "@vercel/analytics/react";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <>
       <Component {...pageProps} />
       <Analytics />
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6121888364111270"></Script>
     </>
   );
 }
