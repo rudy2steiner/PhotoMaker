e4ad1987cb2b066bb7c52aece861c1d51dff1c40
kongmoumou <kongmoumou666@gmail.com>
2023-01-31 18:14:33 +0800

    fix: better style on mobile

--- pages/index.tsx
+++ pages/index.tsx
@@ -82,7 +82,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-2 sm:mt-4">
-        <div className="flex justify-between space-x-5">
+        <div className="flex flex-wrap justify-center space-x-5">
           <a
             className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100 mb-5"
             href="https://twitter.com/shengxj1/status/1619207448547692547"
