690a6a52d75423180d7bfa7e716a559f60ee667a
lvwzhen <lvwzhen@gmail.com>
2023-02-06 18:53:59 +0800

    Add share on twitter

--- components/Header.tsx
+++ components/Header.tsx
@@ -13,28 +13,13 @@ export default function Header() {
       <div>
          <div className="flex flex-wrap justify-center gap-4 items-center">
           <a
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
-            href="https://twitter.com/lvwzhen"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <svg
-              aria-hidden="true"
-              className="h-6 w-6 fill-slate-500 group-hover:fill-slate-700"
-            >
-              <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0 0 22 5.92a8.19 8.19 0 0 1-2.357.646 4.118 4.118 0 0 0 1.804-2.27 8.224 8.224 0 0 1-2.605.996 4.107 4.107 0 0 0-6.993 3.743 11.65 11.65 0 0 1-8.457-4.287 4.106 4.106 0 0 0 1.27 5.477A4.073 4.073 0 0 1 2.8 9.713v.052a4.105 4.105 0 0 0 3.292 4.022 4.093 4.093 0 0 1-1.853.07 4.108 4.108 0 0 0 3.834 2.85A8.233 8.233 0 0 1 2 18.407a11.615 11.615 0 0 0 6.29 1.84" />
-            </svg>
-            <p>Twitter</p>
-          </a>
-
-          <a
-            className="flex max-w-fit items-center justify-center space-x-2 rounded-full border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-md transition-colors hover:bg-gray-100"
+            className="flex max-w-fit items-center justify-center space-x-2 rounded-md border border-gray-300 bg-white px-4 py-2 text-sm text-gray-600 shadow-sm transition-colors hover:bg-gray-100"
             href="https://github.com/lvwzhen/teach-anything"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Github />
-            <p>GitHub</p>
+            <p>Star on GitHub</p>
           </a>
           <a href="https://www.buymeacoffee.com/lvwzhen" target="_blank"><img className="h-[38px]" src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" /></a>
         </div>
