7d790afbdc9b89d1dd5f0bdd4e98746e68d8d9d2
lvwzhen <lvwzhen@gmail.com>
2023-11-27 22:01:34 +0800

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -9,21 +9,10 @@ export const config = {
   runtime: "edge",
 };
 
-const MAX_REQUESTS_PER_DAY = 10;
-
 const handler = async (req: NextRequest): Promise<Response> => {
-  const requestCountCookie = req.cookies.get('requestCount');
-  let requestCount = requestCountCookie ? parseInt(requestCountCookie) : 0;
-
-  if (isNaN(requestCount)) {
-    requestCount = 0;
-  }
-
-  if (requestCount >= MAX_REQUESTS_PER_DAY) {
-    return new Response("Rate limit exceeded", { status: 429 });
-  }
-
-  const { prompt } = (await req.json()) as { prompt?: string };
+  const { prompt } = (await req.json()) as {
+    prompt?: string;
+  };
 
   if (!prompt) {
     return new Response("No prompt in the request", { status: 400 });
@@ -42,12 +31,7 @@ const handler = async (req: NextRequest): Promise<Response> => {
   };
 
   const stream = await OpenAIStream(payload);
-
-  requestCount += 1;
-  const response = new Response(stream);
-  response.headers.append('Set-Cookie', `requestCount=${requestCount}; Max-Age=86400; Path=/; HttpOnly`);
-
-  return response;
+  return new Response(stream);
 };
 
 export default handler;
